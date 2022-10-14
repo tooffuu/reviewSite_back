817c0322fa7303fa0fbc8448f3eaa93688b4776c
tooffuu <thdud8866@naver.com>
2022-10-14 18:42:30 +0900

    install spring security

--- src/main/java/com/project/reviewSite_backend/user/service/UserService.java
+++ src/main/java/com/project/reviewSite_backend/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.project.reviewSite_backend.user.dao.UserRepository;
 import com.project.reviewSite_backend.user.domain.User;
 import com.project.reviewSite_backend.user.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +13,17 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     public void joinUser(UserDto userDto) {
         User user = User.builder()
-                .userid(userDto.getUserid())
                 .username(userDto.getUsername())
-                .password(userDto.getPassword1())
-                .email(userDto.getEmail())
                 .nickname(userDto.getNickname())
+                .userid(userDto.getUserid())
+                .password(passwordEncoder.encode(userDto.getPassword1()))
+                .email(userDto.getEmail())
                 .build();
+
         userRepository.save(user);
     }
 }
